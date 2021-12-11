9dbd63cd84ff9ac653ac744cd1a30f57cb6b30cc
GirijaNaik <94947011+GirijaNaik@users.noreply.github.com>
2021-12-11 21:41:11 +0530

    Create api.py

---
+++ Netflix/api.py
@@ -0,0 +1,39 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .models import CustomUser
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer, LogoutSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+
+class LoginAPI(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = LoginSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+
+class LogoutAPI(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = LogoutSerializer
+
