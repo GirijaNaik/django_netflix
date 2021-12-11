64ec856718bc96f6de970ce5c2a40da9cb173234
GirijaNaik <94947011+GirijaNaik@users.noreply.github.com>
2021-12-11 21:40:09 +0530

    Create urls.py

---
+++ Netflix/urls.py
@@ -0,0 +1,17 @@
+from Netflix.views import Home, ProfileList, ProfileCreate,Watch,ShowMovieDetail,ShowMovie
+from django.urls import path
+from .api import RegisterAPI, LoginAPI, LogoutAPI
+
+app_name='Netflix'
+
+urlpatterns = [
+    path('', Home.as_view()),
+    path('api/register/', RegisterAPI.as_view()),
+    path('api/login/', LoginAPI.as_view()),
+    path('api/logout/', LogoutAPI.as_view()),
+    path('profile/', ProfileList.as_view(), name='profile_list'),
+    path('profile/create/', ProfileCreate.as_view(), name='profile_create'),
+    path('watch/<str:profile_id>/', Watch.as_view(), name='watch'),
+    path('movie/detail/<str:movie_id>/', ShowMovieDetail.as_view(), name='show_det'),
+    path('movie/play/<str:movie_id>/', ShowMovie.as_view(), name='play')
+]
