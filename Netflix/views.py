104524cf58a43301f7ea15623b36cb28295b1a7b
GirijaNaik <94947011+GirijaNaik@users.noreply.github.com>
2021-12-11 21:44:18 +0530

    Create views.py

---
+++ Netflix/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render,redirect
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .forms import ProfileForm
+from .models import movie, Profile
+
+
+# Create your views here.
+class Home(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+@method_decorator(login_required, name='dispatch')
+class ProfileList(View):
+
+        def get(self, request, *args, **kwargs):
+            profiles = request.user.profiles.all()
+
+            print(profiles)
+
+            return render(request, 'profileList.html', {
+                'profiles': profiles
+            })
+
+@method_decorator(login_required, name='dispatch')
+class ProfileCreate(View):
+        def get(self, request, *args, **kwargs):
+            form = ProfileForm()
+
+            return render(request, 'profileCreate.html', {
+                'form': form
+            })
+
+        def post(self, request, *args, **kwargs):
+            form = ProfileForm(request.POST or None)
+
+            if form.is_valid():
+                print(form.cleaned_data)
+                profile = Profile.objects.create(**form.cleaned_data)
+                if profile:
+                    request.user.profiles.add(profile)
+                    return redirect(f'/watch/{profile.uuid}')
+
+            return render(request, 'profileCreate.html', {
+                'form': form
+            })
+
+@method_decorator(login_required, name='dispatch')
+class Watch(View):
+        def get(self, request, profile_id, *args, **kwargs):
+            try:
+                profile = Profile.objects.get(uuid=profile_id)
+
+                movies = Movie.objects.filter(age_limit=profile.age_limit)
+
+                try:
+                    showcase = movies[0]
+                except:
+                    showcase = None
+
+                if profile not in request.user.profiles.all():
+                    return redirect(to='App:profile_list')
+                return render(request, 'movieList.html', {
+                    'movies': movies,
+                    'show_case': showcase
+                })
+            except Profile.DoesNotExist:
+                return redirect(to='App:profile_list')
+
+@method_decorator(login_required, name='dispatch')
+class ShowMovieDetail(View):
+        def get(self, request, movie_id, *args, **kwargs):
+            try:
+
+                movie = Movie.objects.get(uuid=movie_id)
+
+                return render(request, 'movieDetail.html', {
+                    'movie': movie
+                })
+            except movie.DoesNotExist:
+                return redirect('App:profile_list')
+
+@method_decorator(login_required, name='dispatch')
+class ShowMovie(View):
+        def get(self, request, movie_id, *args, **kwargs):
+            try:
+
+                movie = Movie.objects.get(uuid=movie_id)
+
+                movie = movie.videos.values()
+
+                return render(request, 'showMovie.html', {
+                    'movie': list(movie)
+                })
+            except movie.DoesNotExist:
+                 return redirect('App:profile_list')
+
+
+
