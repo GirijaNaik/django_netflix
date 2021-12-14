43049f36ad633eb58309152322d099535ddfc439
GirijaNaik <94947011+GirijaNaik@users.noreply.github.com>
2021-12-14 13:31:05 +0530

    Update api.py

--- Netflix/api.py
+++ Netflix/api.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
 from .models import CustomUser
-from .serializers import UserSerializer, RegisterSerializer, LoginSerializer, LogoutSerializer
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer, LogoutSerializer,  MovieSerializer,  MovieViewSerialize
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -40,4 +40,21 @@ class LoginAPI(generics.GenericAPIView):
 class LogoutAPI(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = LogoutSerializer
+    
+class MovieCreateAPI(generics.CreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        Movie = serializer.save()
+        return Response({
+            "name": MovieSerializer(Movie, context=self.get_serializer_context()).data,
+        })
+
+class MovieViewAPI(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieViewSerializer
+
 
