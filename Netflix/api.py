7640ace155be5d4f99f77876845a0cdaea6eca12
GirijaNaik <94947011+GirijaNaik@users.noreply.github.com>
2021-12-18 16:46:45 +0530

    Update api.py

--- Netflix/api.py
+++ Netflix/api.py
@@ -1,10 +1,12 @@
-from rest_framework import generics, permissions
-from rest_framework.response import Response
+from rest_framework import generics, status,  permissions
 from knox.models import AuthToken
-from .models import CustomUser
-from .serializers import UserSerializer, RegisterSerializer, LoginSerializer, LogoutSerializer,  MovieSerializer,  MovieViewSerialize
+from .models import CustomUser, Movie, Upcoming_movie, Top_rated, comedy_movies, romantic_movies, action_movies, horror_movies, kids_movies
+from .serializers import UserSerializer, RegisterSerializer, MovieSerializer, LoginSerializer, MovieViewSerializer, Upcoming_movieSerializer, Upcoming_movieViewSerializer, Top_rated_movieSerializer
+from rest_framework.response import Response
+from .serializers import Top_rated_movieViewSerializer, comedy_moviesViewSerializer, romantic_moviesViewSerializer, action_moviesViewSerializer, horror_moviesViewSerializer, kids_moviesViewSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+
 
 class UserAPI(generics.ListAPIView):
     queryset = CustomUser.object.all()
@@ -56,5 +58,58 @@ class MovieCreateAPI(generics.CreateAPIView):
 class MovieViewAPI(generics.ListAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieViewSerializer
+    
+class Upcoming_moviesAPI(generics.CreateAPIView):
+    queryset = Upcoming_movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        Movie = serializer.save()
+        return Response({
+            "name": Upcoming_movieSerializer(Movie, context=self.get_serializer_context()).data,
+        })
+
+class Upcoming_moviesViewAPI(generics.ListAPIView):
+    queryset = Upcoming_movie.objects.all()
+    serializer_class = Upcoming_movieViewSerializer
+
+class Top_rated_movieAPI(generics.CreateAPIView):
+    queryset = Top_rated.objects.all()
+    serializer_class = Top_rated_movieSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        Movie = serializer.save()
+        return Response({
+            "name": Top_rated_movieSerializer(Movie, context=self.get_serializer_context()).data,
+        })
+
+class Top_rated_movieViewAPI(generics.ListAPIView):
+    queryset = Top_rated.objects.all()
+    serializer_class = Top_rated_movieViewSerializer
+
+class comedy_moviesViewAPI(generics.ListAPIView):
+    queryset = comedy_movies.objects.all()
+    serializer_class = comedy_moviesViewSerializer
+
+class romantic_moviesViewAPI(generics.ListAPIView):
+    queryset = romantic_movies.objects.all()
+    serializer_class = romantic_moviesViewSerializer
+
+class action_moviesViewAPI(generics.ListAPIView):
+    queryset = action_movies.objects.all()
+    serializer_class = action_moviesViewSerializer
+
+class horror_moviesViewAPI(generics.ListAPIView):
+    queryset = horror_movies.objects.all()
+    serializer_class = horror_moviesViewSerializer
+
+class kids_moviesViewAPI(generics.ListAPIView):
+    queryset = kids_movies.objects.all()
+    serializer_class = kids_moviesViewSerializer
+
 
 
