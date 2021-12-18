7eb340bb0b524a21a4b89a9113f0cc45bc4b6394
GirijaNaik <94947011+GirijaNaik@users.noreply.github.com>
2021-12-18 16:41:53 +0530

    Update admin.py

--- Netflix/admin.py
+++ Netflix/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import CustomUser, movie, Profile, video
+from .models import CustomUser, Movie, Profile, Video, Upcoming_movie, Top_rated, comedy_movies, romantic_movies, action_movies, horror_movies, kids_movies
 
 # Register your models here.
 admin.site.register(CustomUser)
-admin.site.register(movie)
-admin.site.register(Profile)
-admin.site.register(video)
+admin.site.register(Movie)
+admin.site.register(Upcoming_movie)
+admin.site.register(Top_rated)
+admin.site.register(comedy_movies)
+admin.site.register(romantic_movies)
+admin.site.register(action_movies)
+admin.site.register(horror_movies)
+admin.site.register(kids_movies)
+admin.site.register(Video)
